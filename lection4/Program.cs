dafb4b5c1fd9cfc49744dbc05755c7030c138539
amikhaleff <amikhaleff@gmail.com>
2022-10-04 11:08:22 +0300

    mod lection4 added seminar6

--- lection4/Program.cs
+++ lection4/Program.cs
@@ -6,32 +6,32 @@
 // table[4,0]   table[4,1]  table[4,2]  table[4,3]  table[4,4]
 
 
-// void FillArray(int[,] tabl)
-// {
-//     for (int i = 0; i < tabl.GetLength(0); i++)//GetLength(0)- получение строк массива
-//     {
-//         for (int j = 0; j < tabl.GetLength(1); j++)//GetLength(1)- получение столбцов массива
-//         {
-//             tabl[i, j] = new Random().Next(1, 11);
-//         }
-//         Console.WriteLine();
-//     }
-// }
+void FillArray(int[,] tabl)
+{
+    for (int i = 0; i < tabl.GetLength(0); i++)//GetLength(0)- получение строк массива
+    {
+        for (int j = 0; j < tabl.GetLength(1); j++)//GetLength(1)- получение столбцов массива
+        {
+            tabl[i, j] = new Random().Next(1, 11);
+        }
+        Console.WriteLine();
+    }
+}
 
-// void PrintArray(int[,] table)
-// {
-//     for (int i = 0; i < table.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < table.GetLength(1); j++)
-//         {
-//             Console.Write($"{table[i, j]} "); // $"{....} - интерполяция
-//         }
-//         Console.WriteLine();
-//     }
-// }
-// int[,] table = new int[4, 5];
-// // тип данных [,] - значит две размерности__имя=   [строки, столбцы]
-// PrintArray(table);
-// FillArray(table);
-// Console.WriteLine();
-// PrintArray(table);
+void PrintArray(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write($"{table[i, j]} "); // $"{....} - интерполяция
+        }
+        Console.WriteLine();
+    }
+}
+int[,] table = new int[4, 5];
+// тип данных [,] - значит две размерности__имя=   [строки, столбцы]
+PrintArray(table);
+FillArray(table);
+Console.WriteLine();
+PrintArray(table);
---
+++ seminar6_C#/Program.cs
@@ -0,0 +1,61 @@
+﻿string[,] matrix = {
+    {" ", " ", " ", " "," "," "},
+    {" ", " ", " ", " "," "," "},
+    {" ", "@", " ", " "," "," "},
+    {" ", " ", " ", "*"," "," "},
+    {" ", " ", " ", " "," "," "},
+    {" ", " ", " ", " "," "," "}};
+int X = 1;
+int Y = 2;
+int count = 0;
+void WriteArrayMatrix(string[,] matrix)
+{
+    for (int y = 0; y < matrix.GetLength(0); y++)
+    {
+        for (int x = 0; x < matrix.GetLength(1); x++)
+        {
+            Console.Write(matrix[y,x] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+void WriteCoordinates(int X, int Y, int count)
+{
+    Console.WriteLine($"X = {X} Y = {Y} Count = {count}");
+}
+int BonusCreator(string[,] matrix, int X, int Y, int count)
+{
+    while(matrix[Y,X] == "*")
+    {
+        count++;
+        matrix[Y,X] = " ";
+        int newCoordBonusY = new Random().Next(0,matrix.GetLength(0));
+        int newCoordBonusX = new Random().Next(0,matrix.GetLength(1));
+        matrix[newCoordBonusY,newCoordBonusX] = "*"; 
+    }
+    return count;
+}
+void Game(string[,] matrix, int X, int Y,int count)
+{
+    while (true)
+    {
+        matrix[Y,X] = " "; // в матрице начинается с Y
+        ConsoleKeyInfo user_keyTab = Console.ReadKey();// переменная для клавиши
+        if(user_keyTab.Key == ConsoleKey.W) Y--;// клавиши W S A D
+        if(user_keyTab.Key == ConsoleKey.S) Y++;
+        if(user_keyTab.Key == ConsoleKey.A) X--;
+        if(user_keyTab.Key == ConsoleKey.D) X++;
+        if(Y < 0) Y = 0;
+        if(X < 0) X = 0;
+        if(X > matrix.GetLength(1)-1) X = matrix.GetLength(1)-1;
+        if(Y > matrix.GetLength(0)-1) Y = matrix.GetLength(0)-1;
+        count = BonusCreator(matrix,X,Y,count);
+        matrix[Y,X] = "@";
+        Console.Clear();
+        WriteArrayMatrix(matrix);
+        WriteCoordinates(X,Y,count);
+    }  
+}
+WriteArrayMatrix(matrix);
+Game(matrix,X,Y,count);
+
